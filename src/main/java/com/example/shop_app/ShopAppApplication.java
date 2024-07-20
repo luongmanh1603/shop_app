7989d9b86297013505bea296d4a21b5aedbf35e8
luongmanh1603 <114908089+luongmanh1603@users.noreply.github.com>
2024-07-20 22:41:02 +0700

    my web ban hang

---
+++ src/main/java/com/example/shop_app/ShopAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.shop_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShopAppApplication.class, args);
+        System.out.println("Start");
+    }
+
+}
---
+++ src/test/java/com/example/shop_app/ShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
